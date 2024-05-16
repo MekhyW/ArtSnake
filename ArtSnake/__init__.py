2409a94cacca1eb1a854827e0b8703679fbedd8c
rafael katri <rkatri20033@gmail.com>
2024-05-16 10:18:36 -0300

    pls work

--- ArtSnake/__init__.py
+++ ArtSnake/__init__.py
@@ -11,10 +11,4 @@ import site, os
 if not os.path.exists('merged_file.tar'):
     merge_parts()
 
-if not os.path.exists('deep-blind-watermark-removal'):
-    site_packages_dir = site.getsitepackages()[0]
-    clone_path = os.path.join(site_packages_dir, 'ArtSnake', 'deep-blind-watermark-removal')
-    os.makedirs(clone_path, exist_ok=True)
-    subprocess.run(['git', 'clone', 'https://github.com/vinthony/deep-blind-watermark-removal.git', clone_path])
-
 __all__ = ['detect_watermark', 'insert_watermark', 'remove_watermark', 'measure_diff', 'poison_image']
