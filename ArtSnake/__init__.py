554769b17c492dd087d9bcb39427d0b0071c7b92
rafael katri <rkatri20033@gmail.com>
2024-05-16 08:42:23 -0300

    private files

--- ArtSnake/__init__.py
+++ ArtSnake/__init__.py
@@ -1,10 +1,10 @@
 import os
-from detect_watermark import *
-from insert_watermark import *
-from remove_watermark import *
-from measure_diff import *
-from poison_image import *
-from merge_chunks import main as merge_parts
+from .detect_watermark import *
+from .insert_watermark import *
+from .remove_watermark import *
+from .measure_diff import *
+from .poison_image import *
+from .merge_chunks import main as merge_parts
 
 if not os.path.exists('merged_file.tar'):
     merge_parts()
