2a7ce37fafb30db840195603f3601fab39276df0
rafael katri <rkatri20033@gmail.com>
2024-05-16 08:29:43 -0300

    fix __init__.py

--- ArtSnake/__init__.py
+++ ArtSnake/__init__.py
@@ -1,4 +1,4 @@
-
+import os
 from detect_watermark import *
 from insert_watermark import *
 from remove_watermark import *
@@ -6,6 +6,7 @@ from measure_diff import *
 from poison_image import *
 from merge_chunks import main as merge_parts
 
-merge_parts()
+if not os.path.exists('merged_file.tar'):
+    merge_parts()
 
 __all__ = ['detect_watermark', 'insert_watermark', 'remove_watermark', 'measure_diff', 'poison_image']
