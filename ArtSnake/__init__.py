d1e0c8843245074f280406ef79e34cc6882d2638
rafael katri <rkatri20033@gmail.com>
2024-05-16 09:57:20 -0300

    clone on init.py

--- ArtSnake/__init__.py
+++ ArtSnake/__init__.py
@@ -5,8 +5,16 @@ from .remove_watermark import *
 from .measure_diff import *
 from .poison_image import *
 from .merge_chunks import main as merge_parts
+import subprocess
+import site, os
 
 if not os.path.exists('merged_file.tar'):
     merge_parts()
+
+if not os.path.exists('deep-blind-watermark-removal'):
+    site_packages_dir = site.getsitepackages()[0]
+    clone_path = os.path.join(site_packages_dir, 'ArtSnake', 'deep-blind-watermark-removal')
+    os.makedirs(clone_path, exist_ok=True)
+    subprocess.run(['git', 'clone', 'https://github.com/vinthony/deep-blind-watermark-removal.git', clone_path])
 
 __all__ = ['detect_watermark', 'insert_watermark', 'remove_watermark', 'measure_diff', 'poison_image']
--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup, find_packages
-import subprocess
-import site, os
 
 def parse_requirements(file_path):
     with open(file_path, 'r') as f:
@@ -22,11 +20,3 @@ setup(
     long_description_content_type='text/markdown',
     include_package_data=True,
 )
-
-site_packages_dir = site.getsitepackages()[0]
-
-clone_path = os.path.join(site_packages_dir, 'ArtSnake', 'deep-blind-watermark-removal')
-
-os.makedirs(clone_path, exist_ok=True)
-
-subprocess.run(['git', 'clone', 'https://github.com/vinthony/deep-blind-watermark-removal.git', clone_path])
